9bf1056f6f469cfc54b6dab66ba5b25b04ba8ad8
Giulio <giuliohome@gmail.com>
2016-04-29 09:09:50 +0200

    Example for http://stackoverflow.com/questions/36930736/building-wpf-application-as-type-of-wpf-class-libarary/

---
+++ WPFLib/WPFLib/ShareMe.xaml.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WPFLib
+{
+	/// <summary>
+	/// Interaction logic for ShareMe.xaml
+	/// </summary>
+	public partial class ShareMe : Window
+	{
+		public ShareMe()
+		{
+			InitializeComponent();
+		}
+	}
+}
